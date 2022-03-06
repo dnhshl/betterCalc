3b8fa5d961de4ea9e2ef95eb7dee5eb561881486
dnhshl <58460904+dnhshl@users.noreply.github.com>
2022-03-06 15:45:23 +0100

    MultiCalc

--- app/src/main/java/com/example/simplecalc/MainActivity.kt
+++ app/src/main/java/com/example/simplecalc/MainActivity.kt
@@ -2,6 +2,8 @@ package com.example.simplecalc
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.RadioButton
 import simplecalc.R
 import simplecalc.databinding.ActivityMainBinding
 import splitties.toast.toast
@@ -12,6 +14,7 @@ class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
 
     private val TAG = "MainActivity"
+    private var calcModus = 1 // +, 2-, 3x, 4/
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -25,7 +28,44 @@ class MainActivity : AppCompatActivity() {
             if(num1.isEmpty() || num2.isEmpty()){
                 toast(getString(R.string.noInput))
             } else {
-                binding.textViewOutput.text = (num1.toInt() + num2.toInt()).toString()
+                when (calcModus) {
+                    1 -> binding.textViewOutput.text = (num1.toInt() + num2.toInt()).toString()
+                    2 -> binding.textViewOutput.text = (num1.toInt() - num2.toInt()).toString()
+                    3 -> binding.textViewOutput.text = (num1.toInt() * num2.toInt()).toString()
+                    4 -> binding.textViewOutput.text = (num1.toInt() / num2.toInt()).toString()
+                }
+            }
+        }
+    }
+
+    fun onRadioButtonClicked(view: View) {
+        if (view is RadioButton) {
+            // Is the button now checked?
+            val checked = view.isChecked
+
+            // Check which radio button was clicked
+            when (view.getId()) {
+                R.id.rb_plus ->
+                    if (checked) {
+                        calcModus = 1
+                        binding.button.text = getString(R.string.plus)
+                    }
+                R.id.rb_minus ->
+                    if (checked) {
+                        calcModus = 2
+                        binding.button.text = getString(R.string.minus)
+                    }
+                R.id.rb_mult ->
+                    if (checked) {
+                        calcModus = 3
+                        binding.button.text = getString(R.string.mult)
+                    }
+                R.id.rb_div ->
+                    if (checked) {
+                        calcModus = 4
+                        binding.button.text = getString(R.string.div)
+                    }
+
             }
         }
     }
