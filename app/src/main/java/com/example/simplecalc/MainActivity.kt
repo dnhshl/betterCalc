d8574b54841b9aeb3e1c6caf97ff62b45975cb89
dnhshl <58460904+dnhshl@users.noreply.github.com>
2022-03-06 14:52:44 +0100

    SimpleCalc

--- app/src/main/java/com/example/simplecalc/MainActivity.kt
+++ app/src/main/java/com/example/simplecalc/MainActivity.kt
@@ -2,7 +2,9 @@ package com.example.simplecalc
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import simplecalc.R
 import simplecalc.databinding.ActivityMainBinding
+import splitties.toast.toast
 
 
 class MainActivity : AppCompatActivity() {
@@ -17,8 +19,14 @@ class MainActivity : AppCompatActivity() {
         setContentView(binding.root)
 
         binding.button.setOnClickListener {
-            val numberstring = binding.editTextInput.text.toString()
-            binding.textViewOutput.text = numberstring
+            val num1 = binding.editTextInput.text.toString()
+            val num2 = binding.editTextInput2.text.toString()
+
+            if(num1.isEmpty() || num2.isEmpty()){
+                toast(getString(R.string.noInput))
+            } else {
+                binding.textViewOutput.text = (num1.toInt() + num2.toInt()).toString()
+            }
         }
     }
 }
