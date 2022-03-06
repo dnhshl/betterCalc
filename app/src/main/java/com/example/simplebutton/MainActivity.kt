8ab088ab4ad0019d1d60467c22dfa3997ec7bbff
dnhshl <58460904+dnhshl@users.noreply.github.com>
2022-03-06 12:47:28 +0100

    SimpleButtonApp in der neuen Version

--- app/src/main/java/com/example/simplebutton/MainActivity.kt
+++ app/src/main/java/com/example/simplebutton/MainActivity.kt
@@ -2,10 +2,28 @@ package com.example.simplebutton
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
+import com.example.simplebutton.databinding.ActivityMainBinding
+import splitties.toast.longToast
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
+    private val TAG = "MainActivity"
+    private var clickcounter = 0
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.button.setOnClickListener {
+            clickcounter++
+            Log.i(TAG, "SimpleButton wurde $clickcounter mal gedrückt")
+            longToast("SimpleButton wurde $clickcounter mal gedrückt")
+            binding.button.text = getString(R.string.simpleButtonTxt2)
+        }
     }
 }
